821fee01f223a0fec8370d94adfdcaca2e77b5aa
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-07-02 19:30:00 +0530

    Update Ch3_pgm_03.js

--- 03_JS_Objects/Ch3_pgm_03.js
+++ 03_JS_Objects/Ch3_pgm_03.js
@@ -1,16 +1,23 @@
 // A book as an object
 
 var book;
+var book2;
 
 book = {
-    title : "The Hobbit",
+    title : "The Hobbit, or There and Back Again",
     author : "J. R. R. Tolkien",
     published : 1937
 };
 
 console.log(book);
 
+book2={
+    title:"The Suitable Boy",
+    author:"Vikram Seth",
+    published:1993
+};
 
+console.log(book2);
 
 /* Further Adventures
  *
