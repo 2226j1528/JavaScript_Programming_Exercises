9ff58258af53e7ccfcecae4eff0e5a4e24499fe1
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-07-13 19:29:41 +0530

    Update Ch5_pgm_11.js

--- 05_JS_Arguments/Ch5_pgm_11.js
+++ 05_JS_Arguments/Ch5_pgm_11.js
@@ -3,11 +3,25 @@
 var showPlayerPlace;
 
 showPlayerPlace = function (playerName, playerPlace) {
-    console.log(playerName + " is in " + playerPlace);
+    console.log(playerName[0] + " is in " + playerPlace); 
 };
 
 showPlayerPlace("Kandra", "The Dungeon of Doom");
 showPlayerPlace("Dax", "The Old Library");
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName[1] + " is in " + playerPlace); 
+};
+
+showPlayerPlace("Kandra", "The Dungeon of Doom");
+showPlayerPlace("Dax", "The Old Library");
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName[3] + " is in " + playerPlace);
+};
+
+showPlayerPlace("Kandra", "The Dungeon of Doom");
+showPlayerPlace("Dax", "The Old Library");//Error Happens Because there is no third name.
 
 
 
