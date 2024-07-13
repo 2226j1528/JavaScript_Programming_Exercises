26337538b4998f0231f6f9723d4a7d1441114d18
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-07-13 19:06:45 +0530

    Update Ch5_pgm_07.js

--- 05_JS_Arguments/Ch5_pgm_07.js
+++ 05_JS_Arguments/Ch5_pgm_07.js
@@ -4,6 +4,8 @@ var showPlayerName;
 
 showPlayerName = function (playerName) {
     console.log(playerName);
+    console.log("The name has " + playerName.length + " letters.");
+
 };
 
 showPlayerName("Kandra");
