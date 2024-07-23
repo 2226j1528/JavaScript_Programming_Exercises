8451cd9e63ab39b9f19bc92d67492fe411a8e067
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-07-23 20:04:00 +0530

    Update Ch7_pgm_08.js

--- 07_JS_Object_Arguments/Ch7_pgm_08.js
+++ 07_JS_Object_Arguments/Ch7_pgm_08.js
@@ -2,7 +2,17 @@
 
 var message = "We choose to go to the Moon!";
 
-console.log(message.substr(3, 12));
+console.log(message.substr(21, 5)); 
+console.log(message.substr(15, 9)); 
+console.log(message.substr(0, 9));  
+
+console.log(message.substr(21)); 
+console.log(message.substr(15)); 
+console.log(message.substr(0));  
+
+console.log(message.substr(-5));     
+console.log(message.substr(-10, 4)); 
+console.log(message.substr(15, -2)); 
 
 
 
