13465c1358b66f996cacb7fe58af040f42ab4477
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-07-23 19:50:57 +0530

    Update Ch7_pgm_04.js

--- 07_JS_Object_Arguments/Ch7_pgm_04.js
+++ 07_JS_Object_Arguments/Ch7_pgm_04.js
@@ -4,6 +4,8 @@ var point1;
 var point2;
 var move;
 var showPoint;
+var reflectX;
+var rotate90;
 
 move = function (point, change) {
     return {
@@ -16,13 +18,35 @@ showPoint = function (point) {
     console.log("( " + point.x + " , " + point.y + " )");
 };
 
-point1 = { x : 2, y : 5 };
+reflectX = function (point) {
+    return {
+        x: point.x,
+        y: -point.y
+    };
+};
 
-point2 = move(point1, { x : 4, y : -2 });
+rotate90 = function (point) {
+    return {
+        x: -point.y,
+        y: point.x
+    };
+};
 
+point1 = { x: 2, y: 5 };
+
+point2 = move(point1, { x: 4, y: -2 });
 showPoint(point1);
 console.log("Move 4 across and 2 down");
 showPoint(point2);
+
+var reflected = reflectX(point1);
+console.log("Reflecting point1 in the x-axis");
+showPoint(reflected);
+
+var rotated = rotate90(point1);
+console.log("Rotating point1 by 90 degrees anticlockwise");
+showPoint(rotated);
+
 
 
 
