48d56a1141026800ae2f4accf15981b1ff2cdf85
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-07-23 19:54:41 +0530

    Update Ch7_pgm_05.js

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -7,8 +7,32 @@ var showSmaller = function (num1, num2) {
     console.log(smaller + " is smaller than " + larger);
 };
 
+var showLarger = function (num1, num2) {
+    var larger = Math.max(num1, num2);
+    var smaller = Math.min(num1, num2);
+
+    console.log(larger + " is larger than " + smaller);
+};
+
+var showSmallest = function (num1, num2, num3) {
+    var smallest = Math.min(num1, num2, num3);
+
+    console.log(smallest + " is the smallest among " + num1 + ", " + num2 + ", and " + num3);
+};
+
 showSmaller(12, 3);
 showSmaller(-10, 3);
+showSmaller(25, 42);
+showSmaller(7, 7);
+showSmaller(-5, -20);
+
+showLarger(12, 3);
+showLarger(-10, 3);
+
+showSmallest(12, 3, 7);
+showSmallest(-10, 3, -5);
+showSmallest(0, 0, 0);
+
 
 
 
