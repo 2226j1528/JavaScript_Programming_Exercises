5af9eefe6d5c6d3958bdf3a8e934332886785ce4
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-07-28 20:40:47 +0530

    Update Ch8_pgm_12.js

--- 08_JS_Arrays/Ch8_pgm_12.js
+++ 08_JS_Arrays/Ch8_pgm_12.js
@@ -1,32 +1,52 @@
 // Displaying a multiple choice question
 
 var displayQuestion = function (questionAndAnswer) {
-    var options = [ "A", "B", "C", "D", "E" ];
-    
-    console.log(questionAndAnswer.question);
-    
-    questionAndAnswer.answers.forEach(
-      function (answer, i) {
-        console.log(options[i] + " - " + answer);
-      }
-    );
-  };
-  
-  var question1 = {
-    question : "What is the capital of France?",
-    answers : [
-      "Bordeaux",
-      "F",
-      "Paris",
-      "Brussels"
-    ],
-    correctAnswer : "Paris"
-  };
-  
-  displayQuestion(question1);
-  
-  
-  
+  var options = ["A", "B", "C", "D", "E"];
+
+  console.log(questionAndAnswer.question);
+
+  questionAndAnswer.answers.forEach(function (answer, i) {
+    console.log(options[i] + " - " + answer);
+  });
+};
+
+var question1 = {
+  question: "What is the capital of France?",
+  answers: [
+    "Bordeaux",
+    "Lyon",
+    "Paris",
+    "Brussels"
+  ],
+  correctAnswer: "Paris"
+};
+
+var question2 = {
+  question: "Which planet is known as the Red Planet?",
+  answers: [
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn"
+  ],
+  correctAnswer: "Mars"
+};
+
+var question3 = {
+  question: "What is the largest ocean on Earth?",
+  answers: [
+    "Atlantic Ocean",
+    "Indian Ocean",
+    "Arctic Ocean",
+    "Pacific Ocean"
+  ],
+  correctAnswer: "Pacific Ocean"
+};
+
+var questions = [question1, question2, question3];
+
+questions.forEach(displayQuestion);
+
   /* Further Adventures
    *
    * 1) Add two more question objects.
