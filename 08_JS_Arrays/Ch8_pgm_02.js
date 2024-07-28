f06d6def9104f90afe38cf956663efd3720e1826
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-07-28 19:10:09 +0530

    Update Ch8_pgm_02.js

--- 08_JS_Arrays/Ch8_pgm_02.js
+++ 08_JS_Arrays/Ch8_pgm_02.js
@@ -1,14 +1,21 @@
 // Using existing objects in an array
 
-var place1 = { name : "The Pyramids", country : "Egypt" };
-var place2 = { name : "The Grand Canyon", country : "USA" };
-var place3 = { name : "Bondi Beach", country : "Australia" };
+var place1 = { name: "The Pyramids", country: "Egypt" };
+var place2 = { name: "The Grand Canyon", country: "USA" };
+var place3 = { name: "Bondi Beach", country: "Australia" };
 
 var thisYear = [ place1, place2 ];
 var nextYear = [ place3 ];
 
-console.log(thisYear);
-console.log(nextYear);
+console.log("Before adding place1 to nextYear:");
+console.log("thisYear:", thisYear);
+console.log("nextYear:", nextYear);
+
+nextYear.push(place1);
+
+console.log("After adding place1 to nextYear:");
+console.log("thisYear:", thisYear);
+console.log("nextYear:", nextYear);
 
 
 
