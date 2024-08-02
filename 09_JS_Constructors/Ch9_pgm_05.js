82844c3beac19ab82fde2b748a7124535d552e3c
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-08-02 20:20:03 +0530

    Update Ch9_pgm_05.js

--- 09_JS_Constructors/Ch9_pgm_05.js
+++ 09_JS_Constructors/Ch9_pgm_05.js
@@ -10,13 +10,41 @@ var Planet = function (name, position, type) {
         var info = this.name + ": planet " + this.position;
         info +=  " - " + this.type;
         console.log(info);
-        console.log("Moons: " + this.moons.join(', ') + ".");
+        console.log("Moons: " + (this.moons.length > 0 ? this.moons.join(', ') : "None") + ".");
     };
   
     this.addMoon = function (moon) {
         this.moons.push(moon);
     };
+
+    this.removeMoon = function () {
+        if (this.moons.length > 0) {
+            return this.moons.pop();
+        } else {
+            console.log("No moons to remove.");
+            return null;
+        }
+    };
 };
+
+var planet1 = new Planet("Jupiter", 5, "Gas Giant");
+
+planet1.addMoon("Io");
+planet1.addMoon("Europa");
+
+planet1.showPlanet(); 
+
+var planet2 = new Planet("Saturn", 6, "Gas Giant");
+
+planet2.addMoon("Titan");
+planet2.addMoon("Enceladus");
+planet2.addMoon("Mimas");
+
+planet2.showPlanet(); 
+
+planet2.removeMoon(); 
+planet2.showPlanet(); 
+
 
 var planet = new Planet( "Jupiter", 5, "Gas Giant" );
 
