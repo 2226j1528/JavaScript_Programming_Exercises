cb18a54dc67cea709cadccb4ef6520491e757316
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-08-20 21:26:52 +0530

    Update Ch10_pgm_07.js

--- 10_JS_Conditions/Ch10_pgm_07.js
+++ 10_JS_Conditions/Ch10_pgm_07.js
@@ -1,73 +1,97 @@
 // Checking quiz answers
 
 var getQuiz = function () {
-    var score = 0,
-        qIndex = 0,
-        inPlay = true,
-        questions,
-        next,
-        getQuestion,
-        checkAnswer,
-        submit;
-      
-    questions = [
-      {
-        question: "What is the highest mountain in the world?",
-        answer: "Everest"
-      },
-      {
-        question: "What is the highest mountain in Scotland?",
-        answer: "Ben Nevis"
-      }
-    ];
-    
-    next = function () {
-      qIndex = qIndex + 1;
-      
-      if (qIndex >= questions.length) {
-        inPlay = false;
-        console.log("You have finished the quiz.");
-      }
-    };
-    
-    getQuestion = function () {
-      if (inPlay) {
-        return questions[qIndex].question;
-      } else {
-        return "You have finished the quiz.";
-      }
-    };
-    
-    checkAnswer = function (userAnswer) {
-      if (userAnswer === questions[qIndex].answer) {
-        console.log("Correct!");
-        score = score + 1;
-      } else {
-        console.log("No, the answer is " + questions[qIndex].answer);
-      }
-    };
-    
-    submit = function (userAnswer) {
-      var message = "You have finished the quiz.";
-      
-      if (inPlay) {
-        checkAnswer(userAnswer);
-        next();
-        message = "Your score is " + score + " out of " + qIndex;
-      }
-        
-      return message;
-    };
-    
-    return {
-      quizMe: getQuestion,
-      submit: submit
-    };
+  var score = 0,
+      qIndex = 0,
+      inPlay = true,
+      questions,
+      next,
+      getQuestion,
+      checkAnswer,
+      submit,
+      getHint;
+
+  
+  questions = [
+    {
+      question: "What is the highest mountain in the world?",
+      answer: "Everest",
+      hint: "It's located in Asia."
+    },
+    {
+      question: "What is the highest mountain in Scotland?",
+      answer: "Ben Nevis",
+      hint: "It is located in the Scottish Highlands."
+    }
+
+  ];
+
+  
+  next = function () {
+    qIndex = qIndex + 1;
+
+    if (qIndex >= questions.length) {
+      inPlay = false;
+      console.log("You have finished the quiz.");
+    }
   };
+
   
-  var quiz = getQuiz();
+  getQuestion = function () {
+    if (inPlay) {
+      return questions[qIndex].question;
+    } else {
+      return "You have finished the quiz.";
+    }
+  };
+
   
+  checkAnswer = function (userAnswer) {
+    if (userAnswer === questions[qIndex].answer) {
+      console.log("Correct!");
+      score = score + 1;
+    } else {
+      console.log("No, the answer is " + questions[qIndex].answer);
+    }
+  };
+
   
+  submit = function (userAnswer) {
+    var message = "You have finished the quiz.";
+
+    if (inPlay) {
+      checkAnswer(userAnswer);
+      next();
+      message = "Your score is " + score + " out of " + questions.length;
+    }
+
+    return message;
+  };
+
+  
+  getHint = function () {
+    if (inPlay) {
+      return questions[qIndex].hint;
+    } else {
+      return "You have finished the quiz.";
+    }
+  };
+
+
+  return {
+    quizMe: getQuestion,
+    submit: submit,
+    helpMe: getHint
+  };
+};
+
+var quiz = getQuiz();
+
+console.log(quiz.quizMe()); 
+console.log(quiz.submit("K2"));
+console.log(quiz.helpMe()); 
+console.log(quiz.quizMe()); 
+
   
   /* Further Adventures
    *
