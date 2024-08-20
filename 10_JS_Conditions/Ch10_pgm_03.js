a8ef043acde52d05c19ba95d985607fdb3afeb70
Ahalya S <144411487+2226j1528@users.noreply.github.com>
2024-08-20 20:58:07 +0530

    Update Ch10_pgm_03.js

--- 10_JS_Conditions/Ch10_pgm_03.js
+++ 10_JS_Conditions/Ch10_pgm_03.js
@@ -1,20 +1,20 @@
 // Guess the number - using local scope
 
 var getGuesser = function () {
-    var secret = 8;
-  
-    return function (userNumber) {
-      if (userNumber === secret) {
-        console.log("Well done!");
-      } else {
-        console.log("Unlucky, try again.");
-      }
-    };
+  var secret = 8;
+
+  return function (userNumber) {
+    if (userNumber === secret) {
+      console.log("Well done!"); 
+    } else {
+      console.log("Unlucky, try again."); 
+    }
   };
-  
-  var guess = getGuesser();
-  
-  
+};
+
+var guess = getGuesser(); 
+
+ 
   
   /* Further Adventures
    *
